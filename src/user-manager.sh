f0ae96aeef57843460cd59555a62458cf5e459ec
Rose_Kawila <rosekyalo94@gmail.com>
2024-08-08 10:39:39 +0200

    update-information-updated

--- src/user-manager.sh
+++ src/user-manager.sh
@@ -104,14 +104,104 @@ function patient_menu {
         1)
             echo "View Profile Data"
             if [[ -n $userData ]]; then 
-                data=$(echo "$userData" | awk -F, '{print "EmailAdress:","",$1 "\n","Name:", "", $3,$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}')
+                data=$(echo "$userData" | awk -F, '{print "Email Address:","",$1 "\n","First Name:", "", $3,"Last Name:","",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "HIV Diagnosis Date:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","ART Start Date:" ,"", $9 "\n","Country:","",$10 "\n"}')
                 echo "$data"
             else
                 echo "User not found!"
             fi
             ;;
         2)
-            echo "##### Update Profile Data (UPCOMING Deliverable) ######"
+            echo "Update Profile Data"
+            echo "Edit Information"
+            echo "1. EmailAddress"
+            echo "2. FirstName"
+            echo "3. LastName"
+            echo "4. Enter Date of Birth (YYYY-MM-DD)"
+            echo "5. Are you HIV positive? (yes/no)"
+            echo "6. Enter Diagnosis Date (YYYY-MM-DD)"
+            echo "7. Are you on ART drugs? (yes/no)"
+            echo "8.Enter ART Start Date (YYYY-MM-DD)"
+            echo "9. Country"
+            echo "10. Change Password"
+            read -p "Enter choice:" choice
+            case $choice in
+                1)
+                read -p "Enter Old Email address" OldEmailAddress
+                read -p "Enter New EmailAddress: " NewEmailAddress 
+                sed -i "s/$OldEmailAddress/$NewEmailAddress/g" user-store.txt
+                echo "Email address updated!" 
+                grep "$NewEmailAddress" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                2)
+                read -p "Enter Old FirstName: " OldFirstName
+                read -p "Enter New FirstName: " NewFirstName 
+                sed -i "s/$OldFirstName/$NewFirstName/g" user-store.txt
+                echo ""
+                echo "First Name updated!" 
+                echo ""
+                grep "$NewFirstName" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                3)
+                read -p "Enter Old LastName: " OldLastName
+                read -p "Enter New LastName: " NewLastName 
+                sed -i "s/$OldLastName/$NewLastName/g" user-store.txt
+                echo "Last Name updated!" 
+                grep "$NewLastName" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                4)
+                read -p "Enter Old Date of Birth: " OldDOB
+                read -p "Enter New Date of Birth: " NewDOB 
+                sed -i "s/$OldDOB/$NewDOB/g" user-store.txt
+                echo "Date of Birth updated!" 
+                grep "$NewDOB" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                5)
+                read -p "Enter Old HIV Status: " OldHIVStatus
+                read -p "Enter New HIV Status: " NewHIVStatus 
+                sed -i "s/$OldHIVStatus/$NewHIVStatus/g" user-store.txt
+                echo "HIV Status updated!" 
+                grep "$OldHIVStatus" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                6)
+                read -p "Enter Old Diagnosis Date: " OldDiagnosisDate
+                read -p "Enter New Diagnosis Date: " NewDiagnosisDate
+                sed -i "s/$OldDiagnosisDate/$NewDiagnosisDate/g" user-store.txt
+                echo "Diagnosis date updated!" 
+                grep "$NewDiagnosisDate" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "/n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                7)
+                read -p "Enter Old ART Status: " OldARTStatus
+                read -p "Enter New ART Status: " NewARTStatus
+                sed -i "s/$OldARTStatus/$NewARTStatus/g" user-store.txt
+                echo "ART status updated!" 
+                grep "$NewARTStatus" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                8)
+                read -p "Enter Old ART Start Date: " OldARTStartDate
+                read -p "Enter New ART Start Date: " NewARTStartDate 
+                sed -i "s/$OldARTStartDate/$NewARTStartDate/g" user-store.txt
+                echo "ART start date updated!" 
+                grep "$NewARTStartDate" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                9)
+                read -p "Enter Old Country: " OldCountry
+                read -p "Enter New Country: " NewCountry
+                sed -i "s/$OldCountry/$NewCountry/g" user-store.txt
+                echo "Country updated!" 
+                grep "$NewCountry" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+                10)
+                read -p "Enter Old Password: " OldPassword
+                read -p "Enter New Password: " NewPassword
+                hashedPassword=$(hash_password "$OldPassword")
+                sed -i "s/$OldPassword/$hashedPassword/g" user-store.txt
+                echo "Password changed!" 
+                grep "$NewPassword" user-store.txt | awk -F, '{print "EmailAddress:","",$1 "\n","FirstName:", "", $3 "\n","LastName:","\n",$4 "\n","DOB:" ,"", $5 "\n","HIV status:","", $6 "\n", "DiagnosisDate:" ,"", $7 "\n","ART Status:" ,"", $8 "\n","StartDate:" ,"", $9 "\n","Country:","",$10 "\n"}'
+                ;;
+            *)
+                echo "Invalid choice"
+                ;;
+            esac
             ;;
         3)
             echo "##### Calculate Life Expectancy (UPCOMING Deliverable) ######"
